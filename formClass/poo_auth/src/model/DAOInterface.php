85657fdde92253580b9e942bcee0183614bfdd5f
Emre Yildiz <emre.yildiz@afpa-dev-pompey.fr>
2025-01-23 16:59:15 +0100

    start

--- formClass/poo_auth/src/model/DAOInterface.php
+++ formClass/poo_auth/src/model/DAOInterface.php
@@ -4,10 +4,19 @@ interface DAOInterface {
 
     public function create(AbstractEntity $entity);
     
-    public function readOne(int $id): AbstractEntity;
+    public function readOne(int $id): AbstractEntity;/*
+    {
+        try {
+            $db = Database::getInstance();
+            $stmt = $db->prepare("SELECT * from utilisateurs WHERE id = ?");
+            $stmt->execute([ $id ]);
+            return $stmt->fetch();
+        } catch (PDOException $exc) {
+            exit($exc->getMessage());
+        }
+    }*/
     
-    public function readAll(): array;
-/*{
+    public function readAll(): array;/*{
         try {
             $db = Database::getInstance();
             return $db->query("SELECT * from utilisateurs");
