0e317a0c3a696a5f1fde0d360a90d539df6249fd
Emre Yildiz <emre.yildiz@afpa-dev-pompey.fr>
2025-01-23 16:53:31 +0100

    start

--- formClass/poo_auth/src/model/DAOInterface.php
+++ formClass/poo_auth/src/model/DAOInterface.php
@@ -7,9 +7,17 @@ interface DAOInterface {
     public function readOne(int $id): AbstractEntity;
     
     public function readAll(): array;
+/*{
+        try {
+            $db = Database::getInstance();
+            return $db->query("SELECT * from utilisateurs");
+        } catch (PDOException $exc) {
+            exit($exc->getMessage());
+        }
+    };*/
+
     
     public function update(AbstractEntity $entity): bool;
 
     public function delete(AbstractEntity $entity): bool;
-
 }
